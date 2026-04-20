d61227fc3491107dfebc1e51cd43d3d12cfec791
Angel Silva <silvaangel866@gmail.com>
2026-04-20 10:28:46 -0400

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        ContactService service = new ContactService();
-        Contact contact = new Contact("001", "Angel", "Silva", "1234567890", "123 Main Street");
-        service.addContact(contact);
-        System.out.println(service.getContact("001").getFirstName());
-    }
-}
